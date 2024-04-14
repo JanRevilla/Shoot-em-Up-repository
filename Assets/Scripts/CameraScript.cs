3c2beba2f7fd0affdad99d31b3ed5e85634f67a4
Hyux56 <jrevillam@edu.tecnocampus.cat>
2024-04-14 21:42:28 +0200

    CommitCamera
    he creat el moviment de camera i he creat uns limits per aquesta. també he ajustat les mides del mapa en general tenint més en compte la proporció del personatge.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform John;
+    public GameObject target; // targetear a nuestro jugador
 
-    void Update()
+    private float target_poseX; // posicion x del jugador
+    private float target_poseY; // posicion y del jugador
+
+    private float posX; // posicion x camara
+    private float posY; // posicion y camara
+
+    public float derechaMax; // maximo desplazamiento de la camara (derecha)
+    public float izquierdaMax; // maximo desplazamiento de la camara (izquierda)
+
+    public float alturaMax; // altura maxima de la camara
+    public float alturaMin; // altura minima de la camara
+
+    public float speed; // velocidad de la camara
+    public bool encendida; // camara encendida
+
+    void Awake()
     {
-        if (John != null)
+        posX = target_poseX + derechaMax;
+        posY = target_poseY + alturaMin;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), 1);
+    }
+
+    void MoveCam()
+    {
+        if (encendida)
         {
-            Vector3 position = transform.position;
-            position.x = John.position.x;
-            transform.position = position;
+            if (target)
+            {
+                target_poseX = target.transform.position.x;
+                target_poseY = target.transform.position.y;
+
+                if (target_poseX > derechaMax && target_poseX < izquierdaMax)
+                {
+                    posX = target_poseX +1.0f;
+                }   
+                if (target_poseY < alturaMax && target_poseY > alturaMin)
+                {
+                    posY = target_poseY;
+                }
+            }
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), speed*Time.deltaTime);
+
         }
     }
+    void Update()
+    {
+        MoveCam();
+    }
 }
+

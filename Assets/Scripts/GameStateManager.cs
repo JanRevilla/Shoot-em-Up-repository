753fbd5c980b7424a0bbccd4d7f9ed4cd01a9775
Jan R <jrevillam@edu.tecnocampus.cat>
2024-05-23 20:02:56 +0200

    MENU DE PAUSA
    3 botones. Resume. Restart. Quit

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR;
 
 public enum GameState
 {
     Playing,
-    MainMenu
+    MainMenu,
+    Paused // Añadir el estado de pausa
 }
 
 public class GameStateManager : MonoBehaviour
@@ -16,12 +16,13 @@ public class GameStateManager : MonoBehaviour
 
     public GameObject mainMenuUI;
     public GameObject gameUI;
+    public GameObject pauseMenuUI; // Añadir referencia al menú de pausa
+    public GameObject pauseButtonUI;
 
     private GameState currentState;
 
     private void Awake()
     {
-
         if (Instance == null)
         {
             Instance = this;
@@ -35,7 +36,8 @@ public class GameStateManager : MonoBehaviour
 
     private void Start()
     {
-        ChangeState(GameState.MainMenu);
+        currentState = GameState.MainMenu;
+        Debug.Log("Start called. Current state: " + currentState);
     }
 
     public void ChangeState(GameState newState)
@@ -46,33 +48,48 @@ public class GameStateManager : MonoBehaviour
 
     private void HandleStateChange()
     {
-
         switch (currentState)
         {
             case GameState.Playing:
                 Time.timeScale = 1;
                 mainMenuUI.SetActive(false);
                 gameUI.SetActive(true);
+                pauseMenuUI.SetActive(false); // Asegúrate de que el menú de pausa esté desactivado
                 break;
             case GameState.MainMenu:
                 Time.timeScale = 1;
                 mainMenuUI.SetActive(true);
                 gameUI.SetActive(false);
+                pauseMenuUI.SetActive(false); // Asegúrate de que el menú de pausa esté desactivado
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0; // Pausar el juego
+                pauseMenuUI.SetActive(true); // Activar el menú de pausa
+                gameUI.SetActive(false);
+                mainMenuUI.SetActive(false);
                 break;
         }
-
     }
 
     public void OnPlay()
     {
         ChangeState(GameState.Playing);
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        pauseButtonUI.SetActive(true);
+        Debug.Log("OnPlay called. Current state: " + currentState);
     }
 
     public void OnMainMenu()
     {
         ChangeState(GameState.MainMenu);
+        Debug.Log("OnMainMenu called. Current state: " + currentState);
+    }
+
+    public void OnPause()
+    {
+        pauseButtonUI.SetActive(false);
+        ChangeState(GameState.Paused);
+        Debug.Log("OnPause called. Current state: " + currentState);
     }
 
     public void OnExit()
@@ -81,4 +98,24 @@ public class GameStateManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void OnResume()
+    {
+        pauseButtonUI.SetActive(true);
+        if (currentState == GameState.Paused)
+        {
+            ChangeState(GameState.Playing);
+        }
+        Debug.Log("OnResume called. Current state: " + currentState);
+
+    }
+
+    public void OnRestart()
+    {
+        pauseButtonUI.SetActive(true);
+        PointSystem.points = 0;
+        // Reiniciar el nivel actual
+        ChangeState(GameState.Playing);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("OnRestart called. Current state: " + currentState);
+    }
 }
